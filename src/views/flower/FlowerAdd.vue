5f3be12d05db7e79cafa056a223fb967e2429b53
chao.lan <chao.lan@dmall.com>
2023-09-23 23:49:49 +0800

    包花提报布局调整 & 包花人和采花人下拉选保持

--- src/views/flower/FlowerAdd.vue
+++ src/views/flower/FlowerAdd.vue
@@ -1,41 +1,73 @@
 <template>
   <div class="container">
     <el-form ref="form" :model="form" :rules="rules" label-width="100px">
-      <el-form-item label="包花人">
-        <el-select v-model="form.packageId" placeholder="请选择包花人" filterable clearable>
-          <el-option :label="item.label" :value="item.value" v-for="item in packages" :key="item.value"> </el-option>
-        </el-select>
-      </el-form-item>
-      <el-form-item label="采花人" prop="pickerId">
-        <el-select v-model="form.pickerId" placeholder="请选择采花人" filterable clearable>
-          <el-option :label="item.label" :value="item.value" v-for="item in pickers" :key="item.value"> </el-option>
-        </el-select>
-      </el-form-item>
-      <el-form-item label="品种" prop="categoryId">
-        <el-select v-model="form.categoryId" placeholder="请选择品种" filterable clearable>
-          <el-option :label="item.label" :value="item.value" v-for="item in categorys" :key="item.value"> </el-option>
-        </el-select>
-      </el-form-item>
-      <el-form-item label="规格" prop="specificationId">
-        <el-select v-model="form.specificationId" placeholder="请选择品种" filterable clearable>
-          <el-option :label="item.label" :value="item.value" v-for="item in specifications" :key="item.value"> </el-option>
-        </el-select>
-      </el-form-item>
-      <el-form-item label="包花数量" prop="packageAmount">
-        <el-input-number v-model="form.packageAmount" :min="1" :precision="0"></el-input-number>
-      </el-form-item>
-      <el-form-item label="报损原因" prop="damageReasonId">
-        <el-select v-model="form.damageReasonId" placeholder="请选择报损原因" clearable>
-          <el-option v-for="item in damageReasons" :label="item.label" :value="item.value" :key="item.value"> </el-option>
-        </el-select>
-      </el-form-item>
-      <el-form-item label="报损数量" prop="damageAmount">
-        <el-input-number v-model="form.damageAmount" :min="0" :precision="0"></el-input-number>
-      </el-form-item>
-      <el-form-item>
-        <el-button @click="cancel">取消</el-button>
-        <el-button type="primary" @click="onSubmit">提交</el-button>
-      </el-form-item>
+      <el-row type="flex" justify="space-between" class="row-bg">
+        <el-col>
+          <el-form-item label="包花人" prop="packageId">
+            <el-select v-model="form.packageId" placeholder="请选择包花人" filterable clearable>
+              <el-option :label="item.label" :value="item.value" v-for="item in packages" :key="item.value"> </el-option>
+            </el-select>
+          </el-form-item>
+        </el-col>
+        <el-col>
+          <el-form-item label="采花人" prop="pickerId">
+            <el-select v-model="form.pickerId" placeholder="请选择采花人" filterable clearable>
+              <el-option :label="item.label" :value="item.value" v-for="item in pickers" :key="item.value"> </el-option>
+            </el-select>
+          </el-form-item>
+        </el-col>
+      </el-row>
+      <el-row type="flex" justify="space-between" class="row-bg">
+        <el-col>
+          <el-form-item label="品种" prop="categoryId">
+            <el-select v-model="form.categoryId" placeholder="请选择品种" filterable clearable>
+              <el-option :label="item.label" :value="item.value" v-for="item in categorys" :key="item.value"> </el-option>
+            </el-select>
+          </el-form-item>
+        </el-col>
+        <el-col>
+          <el-form-item label="规格" prop="specificationId">
+            <el-select v-model="form.specificationId" placeholder="请选择品种" filterable clearable>
+              <el-option :label="item.label" :value="item.value" v-for="item in specifications" :key="item.value"> </el-option>
+            </el-select>
+          </el-form-item>
+        </el-col>
+      </el-row>
+
+      <el-row type="flex" justify="space-between" class="row-bg">
+        <el-col>
+          <el-form-item label="包花数量" prop="packageAmount">
+            <el-input-number v-model="form.packageAmount" :min="1" :precision="0"></el-input-number>
+          </el-form-item>
+        </el-col>
+        <el-col>
+          <el-form-item label="报损原因" prop="damageReasonId">
+            <el-select v-model="form.damageReasonId" placeholder="请选择报损原因" clearable>
+              <el-option v-for="item in damageReasons" :label="item.label" :value="item.value" :key="item.value"> </el-option>
+            </el-select>
+          </el-form-item>
+        </el-col>
+      </el-row>
+      <el-row type="flex" justify="space-between" class="row-bg">
+        <el-col>
+          <el-form-item label="报损数量" prop="damageAmount">
+            <el-input-number v-model="form.damageAmount" :min="0" :precision="0"></el-input-number>
+          </el-form-item>
+        </el-col>
+        <el-col>
+          <el-form-item>
+            <el-button @click="cancel">取消</el-button>
+            <el-button type="primary" @click="onSubmit">提交</el-button>
+          </el-form-item>
+        </el-col>
+      </el-row>
+
+      <el-row type="flex" justify="center" class="row-bg">
+        <el-col></el-col>
+
+        <el-col></el-col>
+      </el-row>
+
     </el-form>
   </div>
 </template>
@@ -64,10 +96,10 @@ export default {
       userId: '',
       username: '',
       form: {
-        packageId: '',
-        pickerId: '',
-        categoryId: '',
-        specificationId: '',
+        packageId: undefined,
+        pickerId: undefined,
+        categoryId: undefined,
+        specificationId: undefined,
         packageAmount: 1,
         damageReasonId: undefined,
         damageAmount: 0,
@@ -86,7 +118,7 @@ export default {
     this.userId = getUserId()
     this.username = getUserName()
     if (this.tab === 'local') {
-      this.form.packageId = this.userId
+      this.form.packageId = Number(this.userId)
     }
     const query = this.$route.query
     const title = this.$route.meta.title
@@ -136,7 +168,12 @@ export default {
               this.form.creatorName = this.username
               this.form.created = new Date()
               localStorageManager.unshift(Config.FlowRecordKey + this.userId, createFlowerRecord(this.form))
+
+              const curPackageId = this.form.packageId
+              const curPickerId = this.form.pickerId
               this.resetForm()
+              this.form.packageId = curPackageId
+              this.form.pickerId = curPickerId
               this.$message.success('本地数据提交成功')
             }
           } else {
@@ -167,6 +204,10 @@ export default {
 
 <style lang="less" scoped>
 .container {
-  padding: 20px;
+  padding: 30px;
+
+  .row-bg {
+    padding: 20px 0;
+  }
 }
 </style>
